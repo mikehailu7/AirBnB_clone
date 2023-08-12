5f17a0eec982e7e94e10e87292534dc5b430de7f
mikehailu7 <mikehailu7@gmail.com>
2023-08-12 07:31:04 -0700

    air bnb

--- console.py
+++ console.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-"""Defines the HBnB console."""
+# Authors: mikiasHailu and yared Tsgie
+
+""" This function defines the HBnB console """
+
 import cmd
 import re
 from shlex import split
@@ -11,7 +14,6 @@ from models.city import City
 from models.place import Place
 from models.amenity import Amenity
 from models.review import Review
-
 
 def parse(arg):
     curly_braces = re.search(r"\{(.*?)\}", arg)
@@ -32,11 +34,7 @@ def parse(arg):
 
 
 class HBNBCommand(cmd.Cmd):
-    """Defines the HolbertonBnB command interpreter.
-
-    Attributes:
-        prompt (str): The command prompt.
-    """
+    """ This is the HBNBCommand class """
 
     prompt = "(hbnb) "
     __classes = {
@@ -50,11 +48,14 @@ class HBNBCommand(cmd.Cmd):
             }
 
     def emptyline(self):
-        """Do nothing upon receiving an empty line."""
+
+        """ This is empty line function Do nothing that will not do anything """
+
         pass
 
     def default(self, arg):
-        """Default behavior for cmd module when input is invalid"""
+        """ This function will represent the default state of the cmd module """
+
         argdict = {
                 "all": self.do_all,
                 "show": self.do_show,
@@ -75,18 +76,19 @@ class HBNBCommand(cmd.Cmd):
         return False
 
     def do_quit(self, arg):
-        """Quit command to exit the program."""
+        """ This function will quit the cmd """
+
         return True
 
     def do_EOF(self, arg):
-        """EOF signal to exit the program."""
+        """ This function wil signal the HBNB to quit """
+
         print("")
         return True
 
     def do_create(self, arg):
-        """Usage: create <class>
-        Create a new class instance and print its id.
-        """
+        """ This function will print the id """
+
         argl = parse(arg)
         if len(argl) == 0:
             print("** class name missing **")
@@ -97,9 +99,8 @@ class HBNBCommand(cmd.Cmd):
             storage.save()
 
     def do_show(self, arg):
-        """Usage: show <class> <id> or <class>.show(<id>)
-        Display the string representation of a class instance of a given id.
-        """
+        """ This funcation is representation of a class instance of a given id """
+
         argl = parse(arg)
         objdict = storage.all()
         if len(argl) == 0:
@@ -114,8 +115,8 @@ class HBNBCommand(cmd.Cmd):
             print(objdict["{}.{}".format(argl[0], argl[1])])
 
     def do_destroy(self, arg):
-        """Usage: destroy <class> <id> or <class>.destroy(<id>)
-        Delete a class instance of a given id."""
+        """ This will delete the id """
+
         argl = parse(arg)
         objdict = storage.all()
         if len(argl) == 0:
@@ -131,9 +132,8 @@ class HBNBCommand(cmd.Cmd):
             storage.save()
 
     def do_all(self, arg):
-        """Usage: all or all <class> or <class>.all()
-        Display string representations of all instances of a given class.
-        If no class is specified, displays all instantiated objects."""
+        """ This funcation will displays all instantiated objects if there is no class specified """
+
         argl = parse(arg)
         if len(argl) > 0 and argl[0] not in HBNBCommand.__classes:
             print("** class doesn't exist **")
@@ -147,8 +147,7 @@ class HBNBCommand(cmd.Cmd):
             print(objl)
 
     def do_count(self, arg):
-        """Usage: count <class> or <class>.count()
-        Retrieve the number of instances of a given class."""
+        """ This function will retrieve the count """
         argl = parse(arg)
         count = 0
         for obj in storage.all().values():
@@ -156,12 +155,8 @@ class HBNBCommand(cmd.Cmd):
                 count += 1
         print(count)
 
-    def do_update(self, arg):
-        """Usage: update <class> <id> <attribute_name> <attribute_value> or
-       <class>.update(<id>, <attribute_name>, <attribute_value>) or
-       <class>.update(<id>, <dictionary>)
-        Update a class instance of a given id by adding or updating
-        a given attribute key/value pair or dictionary."""
+    def do_destroy_update(self, arg):
+        """ This function will Update a class instance of a given id by adding or updating """
         argl = parse(arg)
         objdict = storage.all()
 
